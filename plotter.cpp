f1432ac42a88253f31b58206fa5592140e49e21d
Charlie Kapsiak <kapsi002@umn.edu>
2022-07-25 10:25:29 -0500

    Formatting

--- plotter.cpp
+++ plotter.cpp
@@ -23,78 +23,78 @@
 #include <THStack.h>
 
 namespace glob {
-  namespace details {
-    template <typename Compare, typename Iterator,
-              typename ValueType =
+namespace details {
+template <typename Compare, typename Iterator,
+          typename ValueType =
               typename std::iterator_traits<Iterator>::value_type>
-    inline bool match_impl(const Iterator pattern_begin, const Iterator pattern_end,
-                           const Iterator data_begin, const Iterator data_end,
-                           const ValueType zero_or_more,
-                           const ValueType exactly_one) {
-      typedef typename std::iterator_traits<Iterator>::value_type type;
+inline bool match_impl(const Iterator pattern_begin, const Iterator pattern_end,
+                       const Iterator data_begin, const Iterator data_end,
+                       const ValueType zero_or_more,
+                       const ValueType exactly_one) {
+  typedef typename std::iterator_traits<Iterator>::value_type type;
 
-      const Iterator null_itr(0);
+  const Iterator null_itr(0);
 
-      Iterator p_itr = pattern_begin;
-      Iterator d_itr = data_begin;
-      Iterator np_itr = null_itr;
-      Iterator nd_itr = null_itr;
-      for (;;) {
-        if (pattern_end != p_itr) {
-          const type c = *(p_itr);
-          if ((data_end != d_itr) &&
-              (Compare::cmp(c, *(d_itr)) || (exactly_one == c))) {
-            ++d_itr;
-            ++p_itr;
-            continue;
-          } else if (zero_or_more == c) {
-            while ((pattern_end != p_itr) && (zero_or_more == *(p_itr))) {
-              ++p_itr;
-            }
-            const type d = *(p_itr);
-            while ((data_end != d_itr) &&
-                   !(Compare::cmp(d, *(d_itr)) || (exactly_one == d))) {
-              ++d_itr;
-            }
-            np_itr = p_itr - 1;
-            nd_itr = d_itr + 1;
-            continue;
-          }
-        } else if (data_end == d_itr)
-          return true;
-        if ((data_end == d_itr) || (null_itr == nd_itr))
-          return false;
-        p_itr = np_itr;
-        d_itr = nd_itr;
+  Iterator p_itr = pattern_begin;
+  Iterator d_itr = data_begin;
+  Iterator np_itr = null_itr;
+  Iterator nd_itr = null_itr;
+  for (;;) {
+    if (pattern_end != p_itr) {
+      const type c = *(p_itr);
+      if ((data_end != d_itr) &&
+          (Compare::cmp(c, *(d_itr)) || (exactly_one == c))) {
+        ++d_itr;
+        ++p_itr;
+        continue;
+      } else if (zero_or_more == c) {
+        while ((pattern_end != p_itr) && (zero_or_more == *(p_itr))) {
+          ++p_itr;
+        }
+        const type d = *(p_itr);
+        while ((data_end != d_itr) &&
+               !(Compare::cmp(d, *(d_itr)) || (exactly_one == d))) {
+          ++d_itr;
+        }
+        np_itr = p_itr - 1;
+        nd_itr = d_itr + 1;
+        continue;
       }
+    } else if (data_end == d_itr)
       return true;
-    }
-    typedef char char_t;
-    struct cs_match {
-      static inline bool cmp(const char_t c0, const char_t c1) {
-        return (c0 == c1);
-      }
-    };
-    struct cis_match {
-      static inline bool cmp(const char_t c0, const char_t c1) {
-        return (std::tolower(c0) == std::tolower(c1));
-      }
-    };
-  } // namespace details
-  inline bool match(const std::string &s, const std::string &p,
-                    const std::string::value_type match_one_or_more = '*',
-                    const std::string::value_type match_exactly_one = '.') {
-    return details::match_impl<details::cs_match>(
-                                                  std::begin(p), std::end(p), std::begin(s), std::end(s), match_one_or_more,
-                                                  match_exactly_one);
+    if ((data_end == d_itr) || (null_itr == nd_itr))
+      return false;
+    p_itr = np_itr;
+    d_itr = nd_itr;
   }
+  return true;
+}
+typedef char char_t;
+struct cs_match {
+  static inline bool cmp(const char_t c0, const char_t c1) {
+    return (c0 == c1);
+  }
+};
+struct cis_match {
+  static inline bool cmp(const char_t c0, const char_t c1) {
+    return (std::tolower(c0) == std::tolower(c1));
+  }
+};
+} // namespace details
+inline bool match(const std::string &s, const std::string &p,
+                  const std::string::value_type match_one_or_more = '*',
+                  const std::string::value_type match_exactly_one = '.') {
+  return details::match_impl<details::cs_match>(
+      std::begin(p), std::end(p), std::begin(s), std::end(s), match_one_or_more,
+      match_exactly_one);
+}
 
-  inline bool isGlob(const std::string &s,
-                     const std::string::value_type match_one_or_more = '*',
-                     const std::string::value_type match_exactly_one = '.') {
-    return s.find(match_exactly_one) != std::string::npos ||
-      s.find(match_one_or_more) != std::string::npos;
-  }
+inline bool isGlob(const std::string &s,
+                   const std::string::value_type match_one_or_more = '*',
+                   const std::string::value_type match_exactly_one = '.') {
+  return s.find(match_exactly_one) != std::string::npos ||
+         s.find(match_one_or_more) != std::string::npos;
+}
 } // namespace glob
 
 struct DataSource {
@@ -126,7 +126,7 @@ struct HistogramInfo {
   std::string title, xlabel, ylabel;
   std::string form;
   std::string mode;
-  bool log_x = false,log_y = false;
+  bool log_x = false, log_y = false;
 
   nlohmann::json data;
 };
@@ -177,14 +177,15 @@ struct Processor {
   virtual void createVisual(Histogram &h, const DataOptions &opts) = 0;
 };
 
-struct ProcessMaker{
-  std::unordered_map<std::string, std::unique_ptr<Processor>> processors;
-  template<typename T>
-  void addProcessor(const std::string& name){processors[name] = std::unique_ptr<T>(new T());}
-   Processor* getProcessor(const std::string& s){
-        return processors[s];
-        }
-  const Processor* operator[](const std::string& s) const {return processors[s];}
+struct ProcessMaker {
+  std::unordered_map<std::string, std::unique_ptr<Processor> > processors;
+  template <typename T> void addProcessor(const std::string &name) {
+    processors[name] = std::unique_ptr<T>(new T());
+  }
+  Processor *getProcessor(const std::string &s) { return processors[s]; }
+  const Processor *operator[](const std::string &s) const {
+    return processors[s];
+  }
 }
 
 // Histogram makeRatioPlot(TH1* h1, TH1* h2) {
@@ -204,7 +205,6 @@ struct ProcessMaker{
 //   legend->SetHeader("Legend", "C");
 //   return canvas;
 // }
-
 struct SimpleOverlayPlotter : public Processor {
 
   void createVisual(Histogram &h, const DataOptions &opts) override {
@@ -219,8 +219,9 @@ struct SimpleOverlayPlotter : public Processor {
     auto stack = new THStack();
     for (const auto &source : sources) {
       if (!source->file->GetListOfKeys()->Contains(hinstance.name.c_str())) {
-        throw std::runtime_error(
-                                 fmt::format("Could not find histogram {} in data source {} with path {}", hinfo.name, source->name, source->path));
+        throw std::runtime_error(fmt::format(
+            "Could not find histogram {} in data source {} with path {}",
+            hinfo.name, source->name, source->path));
       }
       auto hist = (TH1D *)source->file->Get(hinstance.name.c_str());
       hist->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
@@ -230,8 +231,12 @@ struct SimpleOverlayPlotter : public Processor {
       stack->Add(hist);
       legend->AddEntry(hist, source->name.c_str());
     }
-    if (hinfo.log_x) {h.canvas->SetLogx();}
-    if (hinfo.log_y) {h.canvas->SetLogy();}
+    if (hinfo.log_x) {
+      h.canvas->SetLogx();
+    }
+    if (hinfo.log_y) {
+      h.canvas->SetLogy();
+    }
     stack->Draw("nostack PLC PMC HIST p");
     stack->SetTitle(hinfo.title.c_str());
     stack->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
@@ -246,53 +251,48 @@ struct SimpleOverlayPlotter : public Processor {
   }
 };
 
-
-
-  struct StackPlotter : public Processor {
+struct StackPlotter : public Processor {
   void createVisual(Histogram &h, const DataOptions &opts) override {
-  gStyle->SetPalette(kBird);
-  // gStyle->SetOptStat(0);
-  auto &sources = h.set->sources;
-  const auto &hinfo = h.hinstance->info;
-  const auto &hinstance = *h.hinstance;
-  const auto &data = h.hinstance->hinfo.data;
-  std::unordered_set<std::string> stack,nostack;
+    gStyle->SetPalette(kBird);
+    // gStyle->SetOptStat(0);
+    auto &sources = h.set->sources;
+    const auto &hinfo = h.hinstance->info;
+    const auto &hinstance = *h.hinstance;
+    const auto &data = h.hinstance->hinfo.data;
+    std::unordered_set<std::string> stack, nostack;
 
-  auto legend = new TLegend();
-  bool title_set = false;
-  auto stack = new THStack();
-  auto unstack = new THStack();
-  for (const auto &source : sources) {
-  if (!source->file->GetListOfKeys()->Contains(hinstance.name.c_str())) {
-  throw std::runtime_error(
-  fmt::format("Could not find histogram {}", hinfo.name));
+    auto legend = new TLegend();
+    bool title_set = false;
+    auto stack = new THStack();
+    auto unstack = new THStack();
+    for (const auto &source : sources) {
+      if (!source->file->GetListOfKeys()->Contains(hinstance.name.c_str())) {
+        throw std::runtime_error(
+            fmt::format("Could not find histogram {}", hinfo.name));
+      }
+      auto hist = (TH1D *)source->file->Get(hinstance.name.c_str());
+      hist->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
+      hist->GetYaxis()->SetTitle(hinfo.ylabel.c_str());
+      hist->SetMarkerStyle(kPlus);
+      // hist->Draw("Same PLC PMC");
+      stack->Add(hist);
+      legend->AddEntry(hist, source->name.c_str());
+    }
+    stack->Draw("nostack PLC PMC HIST p");
+    stack->SetTitle(hinfo.title.c_str());
+    stack->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
+    stack->GetYaxis()->SetTitle(hinfo.ylabel.c_str());
+
+    legend->SetX1(0.7);
+    legend->SetY1(0.7);
+    legend->SetX2(0.85);
+    legend->SetY2(0.85);
+    legend->SetHeader("Samples", "C");
+    legend->Draw();
   }
-  auto hist = (TH1D *)source->file->Get(hinstance.name.c_str());
-  hist->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
-  hist->GetYaxis()->SetTitle(hinfo.ylabel.c_str());
-  hist->SetMarkerStyle(kPlus);
-  // hist->Draw("Same PLC PMC");
-  stack->Add(hist);
-  legend->AddEntry(hist, source->name.c_str());
-  }
-  stack->Draw("nostack PLC PMC HIST p");
-  stack->SetTitle(hinfo.title.c_str());
-  stack->GetXaxis()->SetTitle(hinfo.xlabel.c_str());
-  stack->GetYaxis()->SetTitle(hinfo.ylabel.c_str());
+};
 
-  legend->SetX1(0.7);
-  legend->SetY1(0.7);
-  legend->SetX2(0.85);
-  legend->SetY2(0.85);
-  legend->SetHeader("Samples", "C");
-  legend->Draw();
-  }
-  };
-
-
-
-
-std::vector<Histogram> runProcess(const ProcessMaker& pm,
+std::vector<Histogram> runProcess(const ProcessMaker &pm,
                                   const std::vector<HistogramInfo> &hinfos,
                                   const std::vector<DataSource> &sources,
                                   const DataOptions &opts) {
@@ -337,7 +337,7 @@ int main(int argc, char *argv[]) {
   for (const auto &i : data.at("histos")) {
     histograms.push_back(i.get<HistogramInfo>());
   }
-  std::string base_in_path =  data.at("base_in_path");
+  std::string base_in_path = data.at("base_in_path");
 
   for (auto &pair : source_sets) {
     for (const DataSource &source : data_sources) {
@@ -350,7 +350,7 @@ int main(int argc, char *argv[]) {
   std::vector<TFile *> files;
   for (DataSource &file : data_sources) {
     std::string inpath = base_in_path + "/" + file.path;
-  fmt::print("Opening file {}\n", inpath);
+    fmt::print("Opening file {}\n", inpath);
     file.file = TFile::Open((inpath).c_str());
     if (!file.file) {
       throw std::runtime_error(fmt::format("Could not open file {}", inpath));
@@ -359,8 +359,8 @@ int main(int argc, char *argv[]) {
       file.keys.insert(key->GetName());
     }
     if (file.keys.empty()) {
-      throw std::runtime_error(fmt::format("File {} does not contain any keys", inpath));
-      
+      throw std::runtime_error(
+          fmt::format("File {} does not contain any keys", inpath));
     }
   }
 
@@ -382,14 +382,14 @@ int main(int argc, char *argv[]) {
         for (const auto &key : hinfo.sets[i]->sources[0]->keys) {
           if (glob::match(key, hinfo.name)) {
             hinfo.instances.push_back(std::unique_ptr<HistogramInfo::Instance>(
-                                                                               new HistogramInfo::Instance{ hinfo, key, i }));
+                new HistogramInfo::Instance{ hinfo, key, i }));
           }
         }
       }
     } else {
       for (int i = 0; i < hinfo.sets.size(); ++i) {
         hinfo.instances.push_back(std::unique_ptr<HistogramInfo::Instance>(
-                                                                           new HistogramInfo::Instance{ hinfo, hinfo.name, i }));
+            new HistogramInfo::Instance{ hinfo, hinfo.name, i }));
       }
     }
   }
