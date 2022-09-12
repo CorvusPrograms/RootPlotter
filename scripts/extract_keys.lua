708c0c3a9bc707c7f4ce70e7b1574ba19171b734
Charlie Kapsiak <kapsi002@umn.edu>
2022-09-12 13:30:40 -0500

    Fixing key extraction

--- scripts/extract_keys.lua
+++ scripts/extract_keys.lua
@@ -2,11 +2,6 @@ keys = {}
 sources={}
 all_data_sources = {}
 
-print(all_data_sources)
-print("=======================STARTING EXTRACTION")
-collectgarbage()
-print("=======================DONE COLLECTING GARBAGE")
-
 for _,v in pairs(_G) do
    if type(v) == "userdata" then
       if (getmetatable(v) or {}).__name == "sol.DataSource" then
@@ -16,13 +11,10 @@ for _,v in pairs(_G) do
 end
 
 
-print(#all_data_sources)
 
 for k,v in ipairs(all_data_sources) do
    print(v:name())
 end
-print(rpv4:keys())
-
 
 for k,v in ipairs(all_data_sources) do
    print(v:name())
